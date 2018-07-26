e2d4dd48abe606e282156e45d9bc2963da98e8c4
Andrzej Mikulewicz <andrzej.mikulewicz@codete.com>
2018-07-26 15:01:44 +0200

    [ADD] added ip/port; removed tests,views;

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
